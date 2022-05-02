a7f7f0f57aa2e39c4d20dbb7da6181fb41619737
Luke Sutter <lucasmsutter@gmail.com>
2022-05-02 05:54:13 -0500

    finished swipe menu functionality

--- src/components/Cart/CartComponents.js
+++ src/components/Cart/CartComponents.js
@@ -24,15 +24,21 @@ CartComponents.CartCard = function ({ children }) {
 CartComponents.CartItem = function ({ children, qty, itemData, setCartData, cartData }) {
 
   function updateCartData(id, newQty) {
-    const itemCartArrayIndex = cartData.findIndex(element => element.id === id)
+    const itemCartArrayIndex = cartData.findIndex(product => product.id === id)
     const cartCopy = [...cartData]
     if (newQty !== 0) {
-      console.log(cartCopy[itemCartArrayIndex])
       cartCopy[itemCartArrayIndex].quantity = newQty
     } else {
       cartCopy.splice(itemCartArrayIndex, 1)
     }
     setCartData(cartCopy)
+  }
+
+  function textLimiter(text) {
+    if (text.length > 100) {
+      
+    }
+    return text
   }
 
   return (
@@ -55,15 +61,14 @@ CartComponents.CartItem = function ({ children, qty, itemData, setCartData, cart
 
 CartComponents.CartTotal = function CartTotal ({ cartData, data, children }) {
   const [top, setTop] = useState(window.innerHeight - 20)
+  const [tap, setTap] = useState({time: null, direction: null})
 
   const elementRef = useRef(null)
   const initial = useRef({ isMouseDown: false, clickPos: null, elementTop: null, time: NaN })
-  
   const mouseCurrentPos = useRef(null)
-  const timeBetweenDownUp = useRef(NaN)
   const animationID = useRef(null)
   
-  const velocity = useRef(NaN)
+  const velocity = useRef(0)
   const mouseDiffs = useRef([])
 
   const upperTarget = window.innerHeight * 1/4
@@ -88,32 +93,32 @@ CartComponents.CartTotal = function CartTotal ({ cartData, data, children }) {
     }
   }
 
-  const handleMouseDown = (e) => {
-    e.preventDefault()
-    initial.current.time = e.timeStamp
-    initial.current.elementTop = elementRef.current.getBoundingClientRect().y 
-    initial.current.clickPos = e.pageY
-    initial.current.isMouseDown = true
-    mouseCurrentPos.current = e.pageY
+
+
+  const handleMove = (e) => {
+    if (initial.current.isMouseDown) {
+      mouseCurrentPos.current = e.pageY
+
+    }
   }
 
-  const handleMouseMove = (e) => {
-    mouseCurrentPos.current = e.pageY
-  }
-
-  const handleMouseUp = (e) => {
+  const handleTouchUp = (e) => {
     initial.current.isMouseDown = false
     initial.current.clickPos = null
     initial.current.elementTop = null
-    mouseCurrentPos.current = NaN
-    
+    mouseCurrentPos.current = null
+    setTap({time: e.timeStamp - initial.current.time, direction: aboveTransitionLine ? 'down' : 'up'})
+
     let calculatedVelocity = mouseDiffs.current.reduce((accumulator, current) => {
       return accumulator + current
     }, 0)/mouseDiffs.current.length
 
     mouseDiffs.current = []
     
-    if (atUpperTarget || atLowerTarget) {
+    if (tap.time < 1000) {
+      velocity.current = aboveTransitionLine ? velocityMin*2 : -(velocityMin*2)
+
+    } else if(atUpperTarget || atLowerTarget) {
       velocity.current = 0
     } else if (Math.abs(calculatedVelocity) > velocityMin) {
       velocity.current = calculatedVelocity
@@ -128,15 +133,13 @@ CartComponents.CartTotal = function CartTotal ({ cartData, data, children }) {
 
   const addSwipeFunctionality = () => {
     slideFunction()
-    document.addEventListener('mouseup', handleMouseUp)
-    document.addEventListener('mousemove', handleMouseMove)
+    document.addEventListener('mousemove', handleMove)
     isMobileRef.current = false
   }
 
   const removeSwipeFunctionality = () => {
     cancelAnimationFrame(animationID.current)
-    document.removeEventListener('mouseup', handleMouseUp)
-    document.removeEventListener('mousemove', handleMouseMove)
+    document.removeEventListener('mousemove', handleMove)
     isMobileRef.current = true
   }
 
@@ -165,9 +168,9 @@ CartComponents.CartTotal = function CartTotal ({ cartData, data, children }) {
   function slideFunction (oldMouse)  {
     //This below section sets mouseDiffs, which is where velocity comes from, mousediffs is an array of recent differences in mouse position
     //first if checks if more than a minimum time has passed before setting mouse diffs
+
     if (initial.current.isMouseDown) {
       if(mouseCurrentPos.current && oldMouse) {
-
         mouseDiffs.current.push(mouseCurrentPos.current - oldMouse)
       }
       if (mouseDiffs.current.length > 8) {
@@ -180,42 +183,46 @@ CartComponents.CartTotal = function CartTotal ({ cartData, data, children }) {
     aboveTransitionLine = elementRef.current && elementRef.current.getBoundingClientRect().y < window.innerHeight * 2/3
 
     setTop(previous => {
-      if (initial.current.isMouseDown) {
-        //two possible categories of states, mouse is down: 
-        if(mouseCurrentPos.current && mouseCurrentPos.current <= upperTarget) {
-          //I think this is triggering when the mouse is near the bottom even
-          //if mouse is above the upper target for the element keep element at target
-          return upperTarget
-        } else if (mouseCurrentPos.current && mouseCurrentPos.current >= lowerTarget) {
-          //if mouse is below the lower target for the element keep element at target
-          return lowerTarget
-        } else if(mouseCurrentPos.current) {
-          //if you have a mouse position the top should be the difference between the initial top of the element
-          //and the difference between the initial mouse click and the current mouse position
-          return initial.current.elementTop - (initial.current.clickPos - mouseCurrentPos.current) 
-        } else {
-          //fail safe in case
-          return previous
+      if (initial.current.isMouseDown && mouseCurrentPos.current) {
+        const calcTarget = initial.current.elementTop - (initial.current.clickPos - mouseCurrentPos.current) 
+        if(aboveTransitionLine) {
+          return Math.max(upperTarget, calcTarget)
+        } else if (!aboveTransitionLine) {
+          return Math.min(lowerTarget, calcTarget)
         }
+
+        return calcTarget
       } else {
-        //if mouse isn't down
-        if (elementRef.current.getBoundingClientRect().y <= upperTarget) {
-          //first check if the element is at or above the upperTarget and keep it at the upperTarget
-          return upperTarget
-        } else if (elementRef.current.getBoundingClientRect().y >= lowerTarget) {
-          //then check if element is at or below lowerTarget
-          return lowerTarget
-        } else if (velocity.current) {
-          //if the mouse isn't down & the element isn't at a target, it should keep moving towards the target
-          return previous + velocity.current
-        } else {
-          return previous
-        }
+        return previous + velocity.current
       }
     })
 
-    //after you set the top by a velocity, reduce absolute value of velocity
-    if (Math.abs(velocity.current) <= velocityMin) {
+    setTap(previous => {
+      if(aboveTransitionLine && previous.direction === 'up') {
+        return {direction: null, time: null}
+      } else if (!aboveTransitionLine && previous.direction === 'down') {
+        return {direction: null, time: null}
+      } else return previous
+    })
+
+
+    animationID.current = requestAnimationFrame(() => slideFunction(oldMouse))
+    
+  }
+
+  useEffect(() => {
+    //anytime top (of the element) or tap changes, change velocity
+    if(tap.direction !== null) {
+      if(tap.direction === 'up' && elementRef.current.getBoundingClientRect().y >= upperTarget) {
+        //if the element has been tapped and is going up but is not above the upperTarget, velocity is negative
+        velocity.current = -(velocityMin*2)
+      } else if (tap.direction === 'down' && elementRef.current.getBoundingClientRect().y <= lowerTarget) {
+        //the opposite
+        velocity.current = velocityMin*2
+      }
+    } else if (elementRef.current.getBoundingClientRect().y <= upperTarget || elementRef.current.getBoundingClientRect().y >= lowerTarget ) {
+      velocity.current = 0
+    } else if (Math.abs(velocity.current) <= velocityMin) {
       if(aboveTransitionLine) {
         velocity.current = -velocityMin
       } else {
@@ -226,11 +233,7 @@ CartComponents.CartTotal = function CartTotal ({ cartData, data, children }) {
     } else if (velocity.current <= -velocityMin) {
       velocity.current = velocity.current + .3
     }
-
-
-    animationID.current = requestAnimationFrame(() => slideFunction(oldMouse))
-    
-  }
+  }, [top, tap])
 
   function getTotal () {
     return cartData.reduce((accumulator, current) => {
@@ -258,12 +261,11 @@ CartComponents.CartTotal = function CartTotal ({ cartData, data, children }) {
       <PassiveListener
         onTouchStart={handleTouchDown}
         onTouchMove={handleTouchMove}
-        onTouchEnd={handleMouseUp}
+        onTouchEnd={handleTouchUp}
       >
         <Icon 
           icon="akar-icons:circle-chevron-up" 
           className="swipe" 
-          onMouseDown={handleMouseDown}
         />
       </PassiveListener>
       <div className='subCard'>
