af970a407fb121ea213231da6bc1c76407f7327e
Luke Sutter <lucasmsutter@gmail.com>
2022-05-24 07:16:32 -0500

    added console log to allDataUtil

--- netlify/functions/allDataUtil.js
+++ netlify/functions/allDataUtil.js
@@ -2,7 +2,7 @@ const { GoogleSpreadsheet } = require('google-spreadsheet')
 
 module.exports = async () => {
   const doc = new GoogleSpreadsheet('14pE3g88RfeSdg_9R2duMIKuvgdk0zghXqfDoaLgItyA')
-
+  let items
   try {
     await doc.useServiceAccountAuth({
       client_email: 'lukesutter@react-e-commerce-db-349813.iam.gserviceaccount.com',
@@ -15,7 +15,7 @@ module.exports = async () => {
  
   const rows = await sheet.getRows()
 
-  const items = rows.map(row => {
+  items = rows.map(row => {
     let tempObj = {}
     row._rawData.forEach((cell, cellIndex) => {
       tempObj[sheet.headerValues[cellIndex]] = cell
@@ -24,10 +24,10 @@ module.exports = async () => {
     return tempObj
   })
 
-  return items
+
   } catch (e) {
     console.log(e)
   }
-
-
+  console.log(items[0])
+  return items
 }
