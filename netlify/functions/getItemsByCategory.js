1dff06f4290c4846a6face92d91072f7f4647ae8
Luke Sutter <lucasmsutter@gmail.com>
2022-05-24 20:47:59 -0500

    Added Headers

--- netlify/functions/getItemsByCategory.js
+++ netlify/functions/getItemsByCategory.js
@@ -9,5 +9,9 @@ exports.handler = async function (event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify(items),
+    headers: {
+      'Access-Control-Allow-Origin': '*', 
+      'Access-Control-Allow-Credentials': 'true' 
+    },
   };
 }
