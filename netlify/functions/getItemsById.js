78627cee9e9cf60acc7d0ac8b34c110d2e115433
Luke Sutter <lucasmsutter@gmail.com>
2022-05-27 08:31:53 -0500

    added logs

--- netlify/functions/getItemsById.js
+++ netlify/functions/getItemsById.js
@@ -5,6 +5,10 @@ exports.handler = async function (event, context) {
   const idsToMatch = event.queryStringParameters.id.split(',')
   const data = await allDataUtil()
   const items = data.filter(item => idsToMatch.includes(item.id))
+
+  console.log(process.env)
+  console.log(process.env.NODE_ENV)
+  console.log(process.env.process.env.REACT_APP_API_KEY)
   return {
     statusCode: 200,
     body: JSON.stringify({ items }),
