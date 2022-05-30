b4d9f014b69293f62858b8f6945922601ec8d364
Luke Sutter <lucasmsutter@gmail.com>
2022-05-30 09:42:26 -0500

    removed * CORS permissions

--- netlify/functions/getItemsById.js
+++ netlify/functions/getItemsById.js
@@ -12,10 +12,7 @@ exports.handler = async function (event, context) {
     return {
       statusCode: 200,
       body: JSON.stringify({ items: items }),
-      headers: {
-        'Access-Control-Allow-Origin': '*', 
-        'Access-Control-Allow-Credentials': 'true' 
-      },
+  
     }
     
   } catch (error) {
